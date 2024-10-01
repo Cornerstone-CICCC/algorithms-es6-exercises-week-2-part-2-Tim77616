8fd2a422131b8dbcee7ab9157750164b60b16944
TIM77616 <tim.ajagbe@gmail.com>
2024-10-01 11:11:40 -0700

    ex02 completed

--- ex02.js
+++ ex02.js
@@ -1,3 +1,11 @@
 // Find Exercise
 // Instructions: Create an array of at least five numbers. Write code to find the first element that is greater than 10.
 
+# Creating an array with five numbers
+array = [5, 8, 12, 3, 20]
+
+# Finding the first element greater than 10
+first_greater_than_10 = next((x for x in array if x > 10), None)
+
+# Output the result
+print(first_greater_than_10)
